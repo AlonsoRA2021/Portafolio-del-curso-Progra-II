5f771f43306d3beeab26d18cf7dd019053cd63a1
Alonso <g.alonso96@hotmail.com>
2021-10-11 14:28:38 -0600

    Semana 3 Herencia

---
+++ VS Code/Semana3/src/Estudiante.java
@@ -0,0 +1,24 @@
+import Origen.Persona;
+
+public class Estudiante extends Persona{
+    private int nota;
+
+    public Estudiante(){}
+
+    public void setNota(int value){
+        this.me = "";
+        this.nota = value;
+
+    }
+
+    public int getNota(){
+        return this.nota;
+    }
+
+    public  String toString(){
+        this.me = super.toString() + " Nota: " + Integer.toString(this.nota);
+        return this.me;
+    }
+
+    
+}
